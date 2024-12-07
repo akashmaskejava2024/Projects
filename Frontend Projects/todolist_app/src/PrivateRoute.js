f3aac50c8894a38c474f91d1a7271dd96849e0e5
akashmaskejava2024 <akashmaske.java@gmail.com>
2024-12-07 09:41:28 +0530

    Update PrivateRoute.js

--- Frontend Projects/todolist_app/src/PrivateRoute.js
+++ Frontend Projects/todolist_app/src/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 
-const PrivateRoute = (element) => {
+const PrivateRoute = ({element}) => {
     const token = sessionStorage.getItem("token");
     return token ? element : <Navigate to="/" replace />
 

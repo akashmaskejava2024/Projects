04554f158e3f489ab857c551c642673920e432b2
Akash Maske <akashmaske.java@gmail.com>
2025-01-12 13:29:06 +0530

    little bit update in pom.xml

--- Backend API's/API/src/test/java/com/nt/exceptionhandler/GlobalExceptionHandlerTest.java
+++ Backend API's/API/src/test/java/com/nt/exceptionhandler/GlobalExceptionHandlerTest.java
@@ -1,6 +1,5 @@
 package com.nt.exceptionhandler;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -8,10 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -55,7 +52,6 @@ class GlobalExceptionHandlerTest {
 			mockMvc.perform(get("api/categories/1"))
 			.andExpect(MockMvcResultMatchers.status().isNotFound());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -70,7 +66,6 @@ class GlobalExceptionHandlerTest {
 			mockMvc.perform(get("api/products/1"))
 			.andExpect(MockMvcResultMatchers.status().isNotFound());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
